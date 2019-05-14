08f114731c6dbd96fc6c05ee1d72105c0a44c8e5
Dan Walmsley <dan@walms.co.uk>
2019-05-14 08:56:50 +0100

    api changes in avalonia

--- src/AvaloniaEdit/Folding/FoldingMarginMarker.cs
+++ src/AvaloniaEdit/Folding/FoldingMarginMarker.cs
@@ -70,11 +70,9 @@ namespace AvaloniaEdit.Folding
         {
             var margin = (FoldingMargin)Parent;
             var activePen = new Pen(margin.SelectedFoldingMarkerBrush,
-                startLineCap: PenLineCap.Square,
-                endLineCap: PenLineCap.Square);
+                lineCap: PenLineCap.Square);
             var inactivePen = new Pen(margin.FoldingMarkerBrush,
-                startLineCap: PenLineCap.Square,
-                endLineCap: PenLineCap.Square);
+                lineCap: PenLineCap.Square);
             var pixelSize = PixelSnapHelpers.GetPixelSize(this);
             var rect = new Rect(pixelSize.Width / 2,
                                  pixelSize.Height / 2,
